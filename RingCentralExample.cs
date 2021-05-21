2d15d09482aebd75fec9c295a8c2f83f34bf13d7
Tyler Long <tyler4long@gmail.com>
2021-05-21 10:43:50 -0700

    Add RingCentral code

--- RingCentralExample.cs
+++ RingCentralExample.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using RingCentral;
 
 namespace MyAwesomeProject
 {
@@ -19,17 +20,20 @@ namespace MyAwesomeProject
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            var clientId = req.Query["clientId"];
+            var clientSecret = req.Query["clientSecret"];
+            var username = req.Query["username"];
+            var extension = req.Query["extension"];
+            var password = req.Query["password"];
+            
+            var rc = new RestClient(clientId, clientSecret, true);
+            try {
+                await rc.Authorize(username, extension, password);
+                var extInfo = await rc.Restapi().Account().Extension().Get();
+                return new OkObjectResult($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss ffff")}\n\nHello, {extInfo.contact.firstName} {extInfo.contact.lastName}.");
+            } catch(RestException re) {
+                return new OkObjectResult($"{DateTime.Now.ToString("yyyyMMdd HH:mm:ss ffff")}\n\n{Utils.FormatHttpMessage(re.httpResponseMessage, re.httpRequestMessage)}");
+            }
         }
     }
 }
